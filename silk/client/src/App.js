e2e3e5f49275c4f1adbeb6257068714a2ca2bacb
Smita Samanta <smithers@Smitas-MacBook-Air.local>
2019-03-04 11:27:30 +0530

    Added first navbar using material UI

--- silk/client/src/App.js
+++ silk/client/src/App.js
@@ -2,11 +2,18 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Button from '@material-ui/core/Button';
+import AppBar from '@material-ui/core/AppBar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <div>
+        <AppBar color="primary" position="static">
+          <h1>My header</h1>
+        </AppBar>
+
+      </div>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
