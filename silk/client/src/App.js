f11cfd5f38e62b23946694c7046095e08773bdaf
Smita Samanta <smithers@Smitas-MacBook-Air.local>
2019-03-04 11:24:34 +0530

    Added first button using material UI

--- silk/client/src/App.js
+++ silk/client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
 
 class App extends Component {
   render() {
@@ -11,6 +12,9 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          <Button variant="contained" color="primary">
+            Hello World
+          </Button>
           <a
             className="App-link"
             href="https://reactjs.org"
